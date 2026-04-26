cbad9af7eaa82b8a94d651fd61a49b01acc853e1
alvarocaastro <alvarocastro.gal@gmail.com>
2026-04-26 03:11:24 +0200

    Track missing adapters/xfoil/__init__.py

---
+++ src/vpf_analysis/adapters/xfoil/__init__.py
@@ -0,0 +1,4 @@
+from vpf_analysis.adapters.xfoil.xfoil_parser import parse_polar_file
+from vpf_analysis.adapters.xfoil.xfoil_runner_adapter import XfoilRunnerAdapter
+
+__all__ = ["XfoilRunnerAdapter", "parse_polar_file"]
